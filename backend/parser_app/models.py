7e1638b24713d210999e8b2e229f53ef5aafd477
ditrigo <ditrig.master@gmail.com>
2024-06-21 10:51:35 +0300

    restructure, back predict model

--- backend/parser_app/models.py
+++ backend/parser_app/models.py
@@ -1,11 +1,26 @@
 from django.db import models
+import uuid
 
 
 class TelegramMessage(models.Model):
-    channel = models.CharField(max_length=255)
-    message_id = models.IntegerField(unique=True)
+    id = models.AutoField(primary_key=True,)
+    uuid = models.UUIDField(default=uuid.uuid4,
+                            editable=False,)
+    channel = models.CharField(max_length=255,)
+    message_id = models.IntegerField(unique=True,)
     text = models.TextField()
     date = models.DateTimeField()
 
     def __str__(self):
         return f"{self.channel} - {self.message_id}"
+
+
+class Predicts(models.Model):
+    id = models.AutoField(primary_key=True)
+    uuid = models.UUIDField(default=uuid.uuid4,
+                            editable=False,)
+    channel = models.CharField(max_length=255,)
+    prediction = models.TextField(null=True, blank=True,)
+
+    def __str__(self):
+        return f"{self.channel} - {self.prediction}"
